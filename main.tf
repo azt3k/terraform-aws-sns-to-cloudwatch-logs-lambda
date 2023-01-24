076d8d05857a0e55414bbf8a2c2707d96b707d7c
Az Ilari <az@dev-io.us>
2023-01-25 12:41:53 +1300

    added log policy docs

--- main.tf
+++ main.tf
@@ -89,9 +89,36 @@ data "aws_sns_topic" "sns_log_topic" {
 #   create new log_group (if create_log_group set)
 # -----------------------------------------------------------------
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_partition" "current" {}
+data "aws_iam_policy_document" "kms_log_access" {
+  policy_id = "key-policy-cloudwatch"
+  statement {
+    sid = "Enable IAM User Permissions"
+    actions = ["kms:*"]
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [format("arn:%s:iam::%s:root", data.aws_partition.current.partition, data.aws_caller_identity.current.account_id)]
+    }
+    resources = ["*"]
+  }
+  statement {
+    sid = "AllowCloudWatchLogs"
+    actions = ["kms:Encrypt*", "kms:Decrypt*", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:Describe*"]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [format("logs.%s.amazonaws.com", data.aws_region.current.name)]
+    }
+    resources = ["*"]
+  }
+}
+
 resource "aws_kms_key" "sns_logged_item_group_key" {
   count = var.enable_data_encryption ? 1 : 0
-  description = "This key is used to encrypt shutdown_lambda log objects"
+  description = "This key is used to encrypt aws_cloudwatch_log_group.sns_logged_item_group objects"
   deletion_window_in_days = 7
   policy = data.aws_iam_policy_document.kms_log_access.json
 }
